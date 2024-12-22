29859f21b9cfb3f248b992ddc721f76c9771b79e
vunraks <92639703+vunraks@users.noreply.github.com>
2024-12-22 22:26:49 +0500

    Add files via upload

---
+++ dz.js
@@ -0,0 +1,30 @@
+// task1,2
+// let num1 = +prompt("Введите 1 число");
+// let num2 = +prompt("Введите 2 число");
+// alert(num1+num2)
+// console.log(num1+num2)
+
+// task3
+// let num1 = +prompt("конверт в ммили");
+// const a = 0.621371;
+// alert(num1 * a);
+
+// task4
+// let num1 = +prompt("Введите 1 число");
+// let num2 = +prompt("Введите 2 число");
+// alert((num1 + num2) / 2);
+
+// task5
+// const color = prompt("Выберете цвет фона- синий, зеленый или желтый, красный");
+
+// if (color === "синий") {
+//   document.body.style.backgroundColor = "blue";
+// } else if (color === "зеленый") {
+//   document.body.style.backgroundColor = "green";
+// } else if (color === "желтый") {
+//   document.body.style.backgroundColor = "yellow";
+// } else if (color === "красный") {
+//   document.body.style.backgroundColor = "red";
+// } else {
+//   alert("Вы выбрали некорктный цвет. Фон остается без изменений");
+// }
